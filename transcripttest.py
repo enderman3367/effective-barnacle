c96e699c21a7900d630566f3a9dd3475d0a6974b
enderman3367 <96430733+enderman3367@users.noreply.github.com>
2023-09-18 22:56:11 -0500

    V 2.0.0
    - back and forth proper conversation
    - audio streaming from eleven labs
    - chat history
    - optimized code organization

--- transcripttest.py
+++
@@ -1,9 +0,0 @@
-import jax.numpy as jnp
-from whisper_jax import FlaxWhisperPipline
-
-# Instantiate the pipeline with batching and half-precision
-pipeline = FlaxWhisperPipline("openai/whisper-large-v2", dtype=jnp.bfloat16, batch_size=16)
-
-# Use the pipeline to transcribe audio
-text = pipeline("usraudfile.wav")
-print(text)
